8dfd8b2d3871e90e72805dd83c0427df53679ff5
Lewis Kabui <lewis.maina@andela.com>
2016-10-10 21:17:04 +0300

    Customize logic in todo app component

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo';
+
+
 
 @Component({
+  // TodoAppComponent's dependency injector will now recognize TodoService class
+  // as a dependency injection token and return a single instance of TodoService
+  // when we ask for it
   moduleId: module.id,
-  selector: 'app-todo-app',
+  selector: 'todo-app',
   templateUrl: 'todo-app.component.html',
-  styleUrls: ['todo-app.component.css']
+  styleUrls: ['todo-app.component.css'],
+  providers: [TodoService]
 })
-export class TodoAppComponent implements OnInit {
+export class TodoAppComponent {
 
-  constructor() {}
+  newTodo: Todo = new Todo();
 
-  ngOnInit() {
+  // Ask Angular DI system to inject the dependency
+  // associated with dependency injection token 'TodoService'
+  // and assign it to a property called 'todoService'
+  constructor(private todoService: TodoService) {}
+
+  // Service is now available as this.todoService
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
   }
 
 }
